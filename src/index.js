dd9f41bbe353a7d5775ba90e0e994bf20d7d15bd
Maksymilian Majer <maks@itcraftship.com>
2018-02-11 00:36:28 +0100

    Logging current row and col

--- src/index.js
+++ src/index.js
@@ -64,6 +64,7 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
+        console.log(Math.floor(i/3), i%3);
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
