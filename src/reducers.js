fc85323933dae854d488f57edf50eac7240ae571
Maksymilian Majer <maks@itcraftship.com>
2018-02-19 23:08:23 +0100

    Refactored to use reducer composition

--- src/reducers.js
+++ src/reducers.js
@@ -2,58 +2,71 @@ import {StepsOrder} from "./viewStates";
 import {SELECT_SQUARE, SELECT_STEP, SET_STEPS_ORDER} from "./actionTypes";
 
 const initialState = {
-    history: [{
-        squares: Array(9).fill(null),
-        selectedIndex: null
-    }],
+    history: getInitialHistory(),
     xIsNext: true,
     stepNumber: 0,
     stepsOrder: StepsOrder.ASC,
 };
 
-function ticTacToeApp(state = initialState, action) {
+function getInitialHistory() {
+    return [{
+        squares: Array(9).fill(null),
+        selectedIndex: null
+    }];
+}
+
+function stepsOrder(state = StepsOrder.ASC, action) {
     switch (action.type) {
         case SET_STEPS_ORDER:
-            return {...state, stepsOrder: action.order};
-        case SELECT_STEP:
-            return {...state, stepNumber: action.stepNumber, xIsNext: (action.stepNumber % 2) === 0};
-        case SELECT_SQUARE:
-            const history = state.history.slice(0, state.stepNumber + 1);
-            const current = history[history.length - 1];
-            const squares = current.slice();
-            if (calculateWinner(squares) || squares[action.index]) {
-                return;
-            }
-            squares[action.index] = this.state.xIsNext ? 'X' : 'O';
-            return {
-                history: history.concat([{
-                    squares,
-                    selectedIndex: action.index
-                }]),
-                stepNumber: history.length,
-                xIsNext: !this.state.xIsNext,
-            };
+            return action.order;
         default:
-            return state
+            return state;
     }
 }
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return [a, b, c];
-        }
+function stepNumber(state = getInitialHistory(), action) {
+    switch (action.type) {
+        case SELECT_STEP:
+            return action.stepNumber;
+        case SELECT_SQUARE:
+            return state.length;
+        default:
+            return state.length - 1;
     }
-    return null;
+}
+
+function xIsNext(state = getInitialHistory(), action) {
+    switch (action.type) {
+        case SELECT_STEP:
+            return (action.stepNumber % 2) === 0;
+        case SELECT_SQUARE:
+            return (state.length % 2) === 0;
+        default:
+            return ((state.length - 1) % 2) === 0;
+    }
+}
+
+function history(state = getInitialHistory(), action) {
+    switch (action.type) {
+        case SELECT_SQUARE:
+            const history = state.slice(0, state.stepNumber + 1);
+            const current = history[history.length - 1];
+            const squares = current.slice();
+            squares[action.index] = xIsNext(state, action) ? 'X' : 'O';
+            return history.concat([{
+                squares,
+                selectedIndex: action.index
+            }]);
+        default:
+            return state;
+    }
+}
+
+function ticTacToeApp(state = initialState, action) {
+    return {
+        history: history(state.history, action),
+        xIsNext: xIsNext(state.history, action),
+        stepNumber: stepNumber(state.history, action),
+        stepsOrder: stepsOrder(state.stepsOrder, action)
+    };
 }
