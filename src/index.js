5c494cf2ed84090b7432a7bd806002ec4b37eb7b
Maksymilian Majer <maks@itcraftship.com>
2018-02-19 23:22:33 +0100

    Uncommented code responsible for stopping the game

--- src/index.js
+++ src/index.js
@@ -81,9 +81,9 @@ class Game extends React.Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
-        // if (calculateWinner(squares) || squares[i]) {
-        //     return;
-        // }
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{
