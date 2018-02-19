5a5c265d648e005ddf12c645d4959ccf68ebf692
Maksymilian Majer <maks@itcraftship.com>
2018-02-19 22:24:57 +0100

    Removed old squares

--- src/reducers.js
+++ src/reducers.js
@@ -17,13 +17,13 @@ function ticTacToeApp(state = initialState, action) {
         case SELECT_SQUARE:
             const history = state.history.slice(0, state.stepNumber + 1);
             const current = history[history.length - 1];
-            const squares = current.squares.slice();
+            const squares = current.slice();
             if (calculateWinner(squares) || squares[action.index]) {
                 return;
             }
             squares[action.index] = this.state.xIsNext ? 'X' : 'O';
             return {
-                history: history.concat(squares),
+                history: history.concat([squares]),
                 stepNumber: history.length,
                 xIsNext: !this.state.xIsNext,
             };
